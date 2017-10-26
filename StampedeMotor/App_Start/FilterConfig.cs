1c71da20286c34cea62bda35cb9e0426d25dbd58
Seann Hicks <pageboy@hotmail.com>
2017-10-26 11:33:01 -0600

    Add project files.

---
+++ StampedeMotor/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace StampedeMotor
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
